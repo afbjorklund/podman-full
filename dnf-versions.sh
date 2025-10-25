c86e3494b1320e703fb865085f5934bab1ef90e0
Anders F Björklund <anders.f.bjorklund@gmail.com>
2025-10-25 12:13:09 +0200

    Versions for packages and arguments

--- dnf-versions.sh
+++ dnf-versions.sh
@@ -1,2 +1,4 @@
-dnf --quiet list --showduplicates conmon crun netavark slirp4netns catatonit aardvark-dns
+dnf --quiet list --showduplicates conmon crun netavark slirp4netns \*catatonit aardvark-dns
 dnf --quiet list --showduplicates golang rust podman
+release="$(rpm -q --qf "%{NAME}" -qf /etc/os-release)"
+dnf --quiet list --showduplicates --available $release
