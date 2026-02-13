ef200046cae9c8d00a8df58f2aad9eb318905b44
Anders F Björklund <anders.f.bjorklund@gmail.com>
2026-02-13 17:32:02 +0100

    Bump dependencies for rawhide

--- arguments.sh
+++ arguments.sh
@@ -2,7 +2,7 @@
 
 while read -r arg package version repository; do
   package=$(echo "$package" | sed -e 's/_VERSION$//' | tr "A-Z_" "a-z-" | sed -e 's/go/golang/')
-  version=$(echo "$version" | sed -e 's/^v//')
+  version=$(echo "$version" | sed -e 's/^v//' | sed -e 's/rc/~rc/')
   if [ "$package" = "debian" ]; then
 	  continue
   fi
